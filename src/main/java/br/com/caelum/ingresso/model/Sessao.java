52a75d5f61bd75420443fbda64b6e057f571686c
Marcos Roberto Silva <marcosrsilva97@hotmail.com>
2018-10-30 16:15:44 -0300

    Fiz muita coisa

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,27 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao
 {
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+	/**
+	 * @Deprecated hibernate only
+	 */
+	public Sessao(){
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala)
 	{
@@ -26,6 +41,10 @@ public class Sessao
 
 	public LocalTime getHorarioTermino() {
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
+	}
+
+	public LocalTime getHorario() {
+		return horario;
 	}
 
 	public void setHorario(LocalTime horario) {
